ca0f18029f17b68737a1d4c9873aa75e8ae42382
Bharath Gadipally <bharath.gadipally@campsystems.com>
2020-10-01 21:20:56 +0530

    added code for excel import and drag drop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MultiDragDropComponent } from './multi-drag-drop/multi-drag-drop.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { ExcelUploaderComponent } from './excel-uploader/excel-uploader.component';
+import { KeyValuePipe, DatePipe } from '@angular/common';
+import { CheckDatePipe } from './CheckDatePipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MultiDragDropComponent,
+    ExcelUploaderComponent,
+    CheckDatePipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    DragDropModule,
+    MatToolbarModule
   ],
-  providers: [],
+  providers: [KeyValuePipe, CheckDatePipe, DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
